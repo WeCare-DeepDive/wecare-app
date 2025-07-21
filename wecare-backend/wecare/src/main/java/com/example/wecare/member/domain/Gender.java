e2898c16781a24255e3f8bb9bd37304b89041aad
김태현 <70064360+kthowns@users.noreply.github.com>
2025-07-21 11:55:04 +0900

    Revert "[Release] 1차 Release 및 CI/CD 테스트"

--- wecare-backend/wecare/src/main/java/com/example/wecare/member/domain/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.example.wecare.member.domain;
-
-public enum Gender {
-    MALE, FEMALE
-}
--- wecare-backend/wecare/src/main/java/com/example/wecare/redis/service/RedisService.java
+++
@@ -1,9 +0,0 @@
-package com.example.wecare.redis.service;
-
-import java.util.concurrent.TimeUnit;
-
-public interface RedisService {
-    void setValues(String key, String value, long timeout, TimeUnit unit);
-    String getValues(String key);
-    void deleteValues(String key);
-}
