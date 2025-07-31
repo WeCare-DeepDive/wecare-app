c6ba81e68d076e4af49b03c5aac7f1bf5a87d14e
KTH <pni2396@gmail.com>
2025-08-01 06:25:40 +0900

    feat: 사용자 관련 API 로직 수정 및 추가
    - 현재 사용자 정보 조회 API 수정
    - 연결된 사용자에 대한 정보 조회 API 구현

--- wecare-backend/wecare/src/main/java/com/example/wecare/member/controller/MemberController.java
+++ wecare-backend/wecare/src/main/java/com/example/wecare/member/controller/MemberController.java
@@ -1,26 +1,30 @@
 package com.example.wecare.member.controller;
 
 import com.example.wecare.member.dto.MemberResponse;
+import com.example.wecare.member.dto.PartnerResponse;
 import com.example.wecare.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
 public class MemberController {
-
     private final MemberService memberService;
 
-    // MyPage 접근 시
     @GetMapping("/me")
     public ResponseEntity<MemberResponse> getMe() {
-        MemberResponse memberResponse = memberService.getMe();
-        return ResponseEntity.ok(memberResponse);
+        return ResponseEntity.ok(memberService.getMe());
+    }
+
+    @GetMapping("/partner")
+    public ResponseEntity<PartnerResponse> getPartner(
+            @RequestParam Long partnerId
+    ) {
+        return ResponseEntity.ok(memberService.getPartner(partnerId));
     }
 }
--- wecare-backend/wecare/src/main/java/com/example/wecare/member/repository/MemberRepository.java
+++ wecare-backend/wecare/src/main/java/com/example/wecare/member/repository/MemberRepository.java
@@ -1,25 +1,12 @@
 package com.example.wecare.member.repository;
 
 import com.example.wecare.member.domain.Member;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
-
-import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("select m from Member m where m.id = :id")
-    Optional<Member> findByIdWithPessimisticLock(Long id);
-
-    @Query("SELECT m FROM Member m " +
-           "LEFT JOIN FETCH m.guardianConnections gc " +
-           "LEFT JOIN FETCH m.dependentConnections dc " +
-           "WHERE m.id = :id")
-    Optional<Member> findByIdWithConnections(@Param("id") Long id);
+    boolean existsMemberByUsername(String username);
 }
