c09a0dee8ce5d4ab62af1bd98b95798ca7d0d641
KTH <pni2396@gmail.com>
2025-08-01 06:32:24 +0900

    fix: 스키마 수정
    - 시간 타입 Timestamp로 통일
    - routines 관련 테이블 정규화, 테이블 명 변경
    - 도메인 비즈니스 로직에 맞게 schema 수정
    - connection, invitation 도메인 분리에 따른 스키마 수정

--- wecare-backend/wecare/src/main/resources/schema.sql
+++ wecare-backend/wecare/src/main/resources/schema.sql
@@ -6,59 +6,64 @@
 
 -- 1. 사용자 테이블
 CREATE TABLE IF NOT EXISTS members (
-    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    birth_date DATE NOT NULL,
-    created_at DATETIME(6),
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username   VARCHAR(50) NOT NULL UNIQUE,
+    birth_date TIMESTAMP NOT NULL,
     gender     ENUM('FEMALE', 'MALE') NOT NULL,
     name       VARCHAR(50) NOT NULL,
     password   VARCHAR(100) NOT NULL,
     role       ENUM('DEPENDENT', 'GUARDIAN') NOT NULL,
-    updated_at DATETIME(6),
-    username   VARCHAR(50) NOT NULL UNIQUE
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- 2. 보호자-피보호자 연결 테이블
-CREATE TABLE IF NOT EXISTS invitations (
+CREATE TABLE IF NOT EXISTS connections (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     dependent_id      BIGINT NOT NULL,
     guardian_id       BIGINT NOT NULL,
-    created_at        DATETIME(6),
     is_active         TINYINT(1) NOT NULL DEFAULT 1,
     relationship_type ENUM('PARENT', 'GRANDPARENT', 'SIBLING', 'FRIEND', 'RELATIVE', 'OTHER') NOT NULL,
-    PRIMARY KEY (dependent_id, guardian_id),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE (dependent_id, guardian_id),
     FOREIGN KEY (dependent_id) REFERENCES members (id) ON DELETE CASCADE,
     FOREIGN KEY (guardian_id) REFERENCES members (id) ON DELETE CASCADE
 );
 
 -- 3. 루틴 테이블
-CREATE TABLE IF NOT EXISTS routine (
-    id                BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    completed_at      DATETIME(6) DEFAULT NULL,
-    created_at        DATETIME(6) NOT NULL,
-    end_time          DATETIME(6),
+CREATE TABLE IF NOT EXISTS routines (
+    id                BIGINT AUTO_INCREMENT PRIMARY KEY,
+    connection_id BIGINT NOT NULL,
+    start_time        TIMESTAMP NOT NULL,
+    end_time          TIMESTAMP,
+    completed_at      TIMESTAMP,
     is_repeat         TINYINT(1) NOT NULL,
-    start_time        DATETIME(6) NOT NULL,
     title             VARCHAR(255) NOT NULL,
-    type              ENUM('ACTIVITY', 'CUSTOM', 'MEDICATION', 'SUPPLEMENT', 'MEAL') NOT NULL,
-    updated_at        DATETIME(6) NOT NULL,
-    dependent_id      BIGINT NOT NULL,
-    guardian_id       BIGINT NOT NULL,
+    routine_type              ENUM('ACTIVITY', 'CUSTOM', 'MEDICATION', 'SUPPLEMENT', 'MEAL') NOT NULL,
     guardian_memo     TEXT,
     dependent_memo    TEXT,
 
-    -- 알림 관련 컬럼
-    is_enabled            TINYINT(1) NOT NULL DEFAULT 1,
-    notification_type     ENUM('NONE', 'ON_START_TIME', 'ON_END_TIME', 'EVERY_10_MINUTES', 'EVERY_30_MINUTES', 'EVERY_HOUR') DEFAULT NULL,
-    sound_type            ENUM('DEFAULT_SOUND', 'SILENT', 'VIBRATION', 'VOICE_MESSAGE') DEFAULT NULL,
-    voice_message_url     VARCHAR(500) DEFAULT NULL,
-
-    FOREIGN KEY (dependent_id) REFERENCES members (id) ON DELETE CASCADE,
-    FOREIGN KEY (guardian_id) REFERENCES members (id) ON DELETE CASCADE
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (connection_id) REFERENCES connections (id) ON DELETE CASCADE
 );
 
 -- 4. 루틴 반복 요일 테이블
 CREATE TABLE IF NOT EXISTS routine_repeat_days (
+    id BIGINT PRIMARY KEY,
     routine_id BIGINT NOT NULL,
-    day        ENUM('DAILY','MON','TUE','WED','THU','FRI','SAT','SUN') NOT NULL,
-    PRIMARY KEY (routine_id, day),
-    FOREIGN KEY (routine_id) REFERENCES routine (id) ON DELETE CASCADE
+    repeat_day        ENUM('DAILY','MON','TUE','WED','THU','FRI','SAT','SUN') NOT NULL,
+    UNIQUE(routine_id, day),
+    FOREIGN KEY (routine_id) REFERENCES routines (id) ON DELETE CASCADE
+);
+
+-- 5. 알림 관련 테이블
+CREATE TABLE IF NOT EXISTS routine_alerts (
+    routine_id BIGINT PRIMARY KEY,
+    is_active            TINYINT(1) NOT NULL DEFAULT 1,
+    notification_type     ENUM('NONE', 'ON_START_TIME', 'ON_END_TIME', 'EVERY_10_MINUTES', 'EVERY_30_MINUTES', 'EVERY_HOUR') NOT NULL,
+    sound_type            ENUM('DEFAULT_SOUND', 'SILENT', 'VIBRATION', 'VOICE_MESSAGE') NOT NULL,
+    voice_message_url     VARCHAR(500),
+    FOREIGN KEY (routine_id) REFERENCES routines (id) ON DELETE CASCADE
 );
