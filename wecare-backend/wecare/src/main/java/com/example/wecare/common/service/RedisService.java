7925e1cf3c73bedbc6a164e205194521a40a90de
KTH <pni2396@gmail.com>
2025-08-01 22:20:01 +0900

    fix: Redis template 수정
    - TimeUnit 지정

--- wecare-backend/wecare/src/main/java/com/example/wecare/common/service/RedisService.java
+++ wecare-backend/wecare/src/main/java/com/example/wecare/common/service/RedisService.java
@@ -4,13 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 @RequiredArgsConstructor
 public class RedisService {
     private final RedisTemplate<String, String> redisTemplate;
 
     public void setValues(String key, String value, long timeout) {
-        redisTemplate.opsForValue().set(key, value, timeout);
+        redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.MILLISECONDS);
     }
 
     public String getValues(String key) {
