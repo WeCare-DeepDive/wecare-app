58163f6af12a9fa19dbd3423d650a50a46a1abff
KTH <pni2396@gmail.com>
2025-08-01 17:18:45 +0900

    fix: 엔드포인트 수정

--- wecare-backend/wecare/src/main/java/com/example/wecare/connection/controller/ConnectionController.java
+++ wecare-backend/wecare/src/main/java/com/example/wecare/connection/controller/ConnectionController.java
@@ -19,14 +19,14 @@ public class ConnectionController {
         return ResponseEntity.ok(connectionService.getMyConnections());
     }
 
-    @PatchMapping("/deactivate/{targetUserId}")
+    @PatchMapping("/{targetUserId}/deactivate")
     public ResponseEntity<String> deactivateConnection(@PathVariable Long targetUserId) {
         connectionService.deactivateConnection(targetUserId);
 
         return ResponseEntity.ok("연결이 해제되었습니다.");
     }
 
-    @PatchMapping("/reactivate/{targetUserId}")
+    @PatchMapping("/{targetUserId}/reactivate")
     public ResponseEntity<String> reactivateConnection(@PathVariable Long targetUserId) {
         connectionService.reactivateConnection(targetUserId);
 
@@ -34,7 +34,7 @@ public class ConnectionController {
     }
 
     // 연결 정보 수정 ex) 보호자, 피보호자 간 관계 (가족, 친구 등)
-    @PatchMapping("/relationship/{targetUserId}")
+    @PatchMapping("/{targetUserId}/relationship")
     public ResponseEntity<String> relationshipConnection(
             @PathVariable Long targetUserId
     ) {
