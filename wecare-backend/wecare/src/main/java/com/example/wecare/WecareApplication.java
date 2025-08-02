50665842137c716743574695d4ab905c94f9cb7f
KTH <pni2396@gmail.com>
2025-08-03 07:15:05 +0900

    fix: routine 조회 인가 로직 주석

--- wecare-backend/wecare/src/main/java/com/example/wecare/WecareApplication.java
+++ wecare-backend/wecare/src/main/java/com/example/wecare/WecareApplication.java
@@ -1,13 +1,20 @@
 package com.example.wecare;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class WecareApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WecareApplication.class, args);
+    }
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
